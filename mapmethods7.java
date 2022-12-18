9ffd48d7e2495be3bf2573f15c6d50a78e379782
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-19 00:27:17 +0530

    Add files via upload

---
+++ mapmethods7.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class mapmethods7 {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("Three", 3);
+        map.put("Four", 4);
+        map.put("Five", 5);
+        map.put("Six", 6);
+        System.out.println("Before clear:" + map);
+        map.forEach((String key, Integer value) -> {
+            System.out.println(key + ":" + value);
+        });
+
+        int val=  map.getOrDefault("Six", 6);
+        System.out.println("Value :" + val);
+
+        System.out.println(map.getOrDefault("Five", 5));
+
+    }
+    
+}
