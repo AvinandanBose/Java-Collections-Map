b9b7ecd62901634fc7d992b372daed1344ead9e7
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-19 17:35:28 +0530

    Add files via upload

---
+++ mapmethods16.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+public class mapmethods16 {
+    public static void main(String[] args) throws Exception {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        map.put("e", 5);
+        map.put("f", 6);
+        System.out.println("Map:" + map);
+        System.out.println("Map Size:" + map.size());
+        
+    }
+}
