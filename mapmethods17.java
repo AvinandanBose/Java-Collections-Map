69e9ff6adef55167ece08cd80c03ea062ecd10b0
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-19 18:30:16 +0530

    Add files via upload

--- mapmethods17.java
+++ mapmethods17.java
@@ -15,5 +15,44 @@ public class mapmethods17 {
         map.values().forEach((v) -> System.out.println(v));
         System.out.println(" ");
         System.out.println("Values :" + map.values());
+        // remove
+        map.values().remove(2);
+        System.out.println(" ");
+        System.out.println("Map:" + map);
+        System.out.println("Map:" + map.values());
+        System.out.println(" ");
+        // removeAll
+        map.values().removeAll(map.values());
+        System.out.println(map.values());
+        System.out.println(" ");
+
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        map.put("e", 5);
+        map.put("f", 6);
+
+        
+        // removeIf
+        map.values().removeIf((v) -> v % 2 == 0);
+        System.out.println(" ");
+        System.out.println("Map:" + map);
+        System.out.println(" ");
+        //retainAll
+        HashMap<String, Integer> map1 = new HashMap<>();
+        map1.put("a", 3);
+        map1.put("b", 4);
+        map1.put("c", 5);
+        map1.put("d", 2);
+        System.out.println("Map1: " + map1);
+        System.out.println("Map1: "+ map1.values());
+       map.values().retainAll(map1.values());
+        System.out.println(" ");
+        System.out.println("Map:" + map);
+        System.out.println(" ");
+      
+       System.out.println(map.values());
+        
     }
 }
