ef97536094b230ffb3df8b7dcb158e3a60890ff8
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-22 18:12:20 +0530

    Add files via upload

---
+++ synchronizedLinkedHashMap.java
@@ -0,0 +1,35 @@
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+public class synchronizedLinkedHashMap {
+    public static void main(String[] args) throws Exception {
+
+        Thread thread = new Thread();
+
+        Map<String, Integer> map = Collections.synchronizedMap(new LinkedHashMap<>());
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        map.put("e", 5);
+        map.put("f", 6);
+        System.out.println(map);
+       
+        
+        Map<String, Integer> map1 = new LinkedHashMap<>();
+        thread.start();
+        Thread.sleep(1000);
+        synchronized (map1) {
+            map1.put("a", 1);
+            map1.put("b", 2);
+            map1.put("c", 3);
+            map1.put("d", 4);
+            map1.put("e", 5);
+            map1.put("f", 6);
+            System.out.println(map1);
+        }
+        thread.stop();
+        
+    }
+    
+}
