28ee18ccc43386fc7940001e20e986b5c136050f
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-22 22:50:31 +0530

    Add files via upload

---
+++ IdentityHashMap2.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
+public class IdentityHashMap2 {
+    public static void main(String[] args) throws Exception {
+      Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put(new String("a"), 1);
+        
+        System.out.println("Map:" + map);
+        map.put("a", 2);
+        System.out.println("Map:" + map);
+        System.out.println("Size of Map:" + map.size());
+        Map<String, Integer> map1 = new IdentityHashMap<>();
+        map1.put("a", 1);
+        map1.put(new String("a"), 1);
+        System.out.println("Map1:" + map1);
+        map1.put("a", 2);
+        System.out.println("Map1:"+map1);
+        map1.put(new String("a"), 2);
+        System.out.println("Map1:" + map1);
+        System.out.println("Size of Map1:" + map1.size());
+        
+    }
+    
+}
