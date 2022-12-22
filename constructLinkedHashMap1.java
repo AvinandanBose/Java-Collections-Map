8c6f622713e91b6d91c46bb727f65a2e43a6bed6
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-22 16:33:09 +0530

    Add files via upload

---
+++ constructLinkedHashMap1.java
@@ -0,0 +1,14 @@
+
+import java.util.LinkedHashMap;
+public class constructLinkedHashMap1 {
+    public static void main(String[] args) throws Exception {
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>(6);
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        map.put("e", 5);
+        map.put("f", 6);
+        System.out.println(map);
+    }
+}
---
+++ constructLinkedHashMap3.java
@@ -0,0 +1,13 @@
+import java.util.LinkedHashMap;
+public class constructLinkedHashMap3 {
+    public static void main(String[] args) throws Exception {
+         LinkedHashMap<String, Integer> map = new LinkedHashMap<>(6, 0.7f, true);
+         map.put("a", 1);
+         map.put("b", 2);
+         map.put("c", 3);
+         map.put("d", 4);
+         map.put("e", 5);
+         map.put("f", 6);
+         System.out.println(map);
+    }
+}
