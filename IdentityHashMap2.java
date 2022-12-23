4a06e383248764e86e0487563195a8cab267acad
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-12-23 14:29:36 +0530

    Add files via upload

--- IdentityHashMap2.java
+++ IdentityHashMap2.java
@@ -8,16 +8,20 @@ public class IdentityHashMap2 {
         map.put(new String("a"), 1);
         
         System.out.println("Map:" + map);
-        map.put("a", 2);
+        Boolean b = map.put("a", 1).equals(map.put(new String("a"), 1));
+        System.out.println("b:" + b);
         System.out.println("Map:" + map);
         System.out.println("Size of Map:" + map.size());
         Map<String, Integer> map1 = new IdentityHashMap<>();
         map1.put("a", 1);
         map1.put(new String("a"), 1);
+        Boolean c = map1.put("a", 1).equals(map1.put(new String("a"), 1));
+        System.out.println("c:" + c);
+        System.out.println("Map1:" + map1);
+        
+        map1.put(new String("a"), 2);
         System.out.println("Map1:" + map1);
         map1.put("a", 2);
-        System.out.println("Map1:"+map1);
-        map1.put(new String("a"), 2);
         System.out.println("Map1:" + map1);
         System.out.println("Size of Map1:" + map1.size());
         
