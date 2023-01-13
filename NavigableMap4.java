443be3ad148608371a1f1caa3f6d63d516fab32b
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-01-13 22:15:33 +0530

    Add files via upload

---
+++ NavigableMap4.java
@@ -0,0 +1,19 @@
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class NavigableMap4 {
+    // descendingMap
+    public static void main(String[] args) throws Exception {
+        NavigableMap<Float, Integer> map = new TreeMap<>();
+        map.put(1.8f,11 );
+        map.put(2.6f, 9);
+        map.put(3.4f, 78);
+        map.put(4.3f, 5);
+        map.put(5.6f, 1);
+        map.put(6.8f, 3);
+        System.out.println("Map:" + map);
+        System.out.println("Descending Map:" + map.descendingMap());
+        
+    }
+    
+}
