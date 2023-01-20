6f655c2076aaa461d733305411b5fe3a7e198dd7
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-01-20 19:55:56 +0530

    Add files via upload

--- constructConcurrentHashMap4.java
+++ constructConcurrentHashMap4.java
@@ -6,28 +6,63 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 public class constructConcurrentHashMap4 {
     public static void main(String[] args) throws Exception {
 
-        //HashMap
-        Map<String, String> map = new HashMap<>();
+        // ConcurrentHashMap
+        ConcurrentMap<String, String> map = new ConcurrentHashMap<>();
         ConcurrentHashMap<String, String> map1 = new ConcurrentHashMap<>(map);
+
+        /* Similarly.... */
+
+        ConcurrentHashMap<String, String> map11 = new ConcurrentHashMap<>(); 
+        ConcurrentHashMap<String, String> map12 = new ConcurrentHashMap<>(map11);
+
+        //HashMap
+        Map<String, String> map0 = new HashMap<>();
+        ConcurrentHashMap<String, String> map_new = new ConcurrentHashMap<>(map0);
+
+        /*Similarly....*/
+        HashMap<String, String> map_new1 = new HashMap<>();
+        ConcurrentHashMap<String, String> map_new2 = new ConcurrentHashMap<>(map_new1);
+
+
 
         //TreeMap
         Map<String, String> map2 = new TreeMap<>();
         ConcurrentHashMap<String, String> map3 = new ConcurrentHashMap<>(map2);
 
+        /* Similarly.... */
+
+        TreeMap<String, String> map22 = new TreeMap<>();
+        ConcurrentHashMap<String, String> map33 = new ConcurrentHashMap<>(map22);
+
         //LinkedHashMap
+
+        
         Map<String, String> map4 = new LinkedHashMap<>();
         ConcurrentHashMap<String, String> map5 = new ConcurrentHashMap<>(map4);
+
+        /* Similarly.... */
+        LinkedHashMap<String, String> map44 = new LinkedHashMap<>();
+        ConcurrentHashMap<String, String> map55 = new ConcurrentHashMap<>(map44);
 
         //WeakHashMap
         Map<String, String> map6 = new WeakHashMap<>();
         ConcurrentHashMap<String, String> map7 = new ConcurrentHashMap<>(map6);
 
+        /* Similarly.... */
+        WeakHashMap<String, String> map66 = new WeakHashMap<>();
+        ConcurrentHashMap<String, String> map77 = new ConcurrentHashMap<>(map66);
+
         //IdentityHashMap
-        Map<String, String> map8 = new IdentityHashMap<>();
+        IdentityHashMap<String, String> map8 = new IdentityHashMap<>();
         ConcurrentHashMap<String, String> map9 = new ConcurrentHashMap<>(map8);
+
+        /* Similarly.... */
+        IdentityHashMap<String, String> map88 = new IdentityHashMap<>();
+        ConcurrentHashMap<String, String> map99 = new ConcurrentHashMap<>(map88);
 
        
 
@@ -42,6 +77,11 @@ public class constructConcurrentHashMap4 {
         map1.put("8", "eight");
 
         System.out.println("Map:" + map1);
+
+        map_new.putAll(map1);
+        System.out.println("Map_new:" + map_new);
+
+        
     }
     
 }
